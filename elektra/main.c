ebac3a51192506b03c9e1f19fbb5bbf48d7c0753
Beatriz Paiva <beeepaiva@gmail.com>
2018-10-01 19:58:08 -0300

    SMARTGATOR-1 Separando funcoes de telas em .c

--- elektra/main.c
+++ elektra/main.c
@@ -127,7 +127,31 @@ int main(void)
 				}
 			}break;
 			case 2:
-				telaOpcoes(evento, janela);
+				if (evento.type == ALLEGRO_EVENT_MOUSE_AXES)
+				{
+					int btn_voltar = botoesMenuIniciar(evento, 30, 90, 90, 140);
+
+					if (btn_voltar)
+					{
+						al_set_system_mouse_cursor(janela, ALLEGRO_SYSTEM_MOUSE_CURSOR_LINK);
+					}
+					else
+					{
+						al_set_system_mouse_cursor(janela, ALLEGRO_SYSTEM_MOUSE_CURSOR_DEFAULT);
+					}
+
+				}
+				else if (evento.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP)
+				{
+					int btn_voltar = botoesMenuIniciar(evento, 30, 90, 90, 140);
+
+					if (btn_voltar)
+					{
+						al_draw_bitmap(IMAGEM_MENUINICIAL, 0, 0, 0);
+						al_flip_display();
+						return;
+					}
+				}
 				break;
 			case 3:
 				al_draw_bitmap(IMAGEM_MENUINICIAL, 0, 0, 0);
