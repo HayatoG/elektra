977f9ea81a1fc9032afe31ade07755dd4af84596
Guilherme <guilherme.ryders@gmail.com>
2018-10-15 05:07:52 -0300

    SMARTGATOR-2 - Funcoes.c não funcionaram, mantive-as no main.c. Estarei verificando isso com o decorrer do jogo

--- elektra/Funcoes.c
+++ elektra/Funcoes.c
@@ -1,4 +1,6 @@
 #include <allegro5/allegro.h>
-int botoesMenuIniciar(ALLEGRO_EVENT e, int x1, int x2, int y1, int y2) {
+#include <stdio.h>
+
+/*int botoesMenuIniciar(ALLEGRO_EVENT e, int x1, int x2, int y1, int y2) {
 	return e.mouse.x >= x1 && e.mouse.x <= x2 && e.mouse.y >= y1 && e.mouse.y <= y2;
-}
+}*/
