feb5f0afb00dbdab6dfe39e742d2ab9ea029d978
Guilherme <guilherme.ryders@gmail.com>
2018-10-22 06:37:37 -0300

    Reajustando 'Constantes.h'

--- elektra/Constantes.h
+++ elektra/Constantes.h
@@ -8,25 +8,7 @@
 const int LARGURA_TELA = 1280;
 const int ALTURA_TELA = 720;
 //Constantes devem ser declaradas com letras maiusculas
-/*
-extern ALLEGRO_DISPLAY *janela = NULL;
-extern ALLEGRO_BITMAP *IMAGEM_SPLASHSCREEN = NULL;
-extern ALLEGRO_BITMAP *IMAGEM_MENUINICIAL = NULL;
-extern ALLEGRO_BITMAP *IMAGEM_TELAOPCOES = NULL;
-extern ALLEGRO_BITMAP *IMAGEM_FASE1 = NULL;
-extern ALLEGRO_BITMAP *IMAGEM_FASE2 = NULL;
-extern ALLEGRO_BITMAP *IMAGEM_FASE3 = NULL;
-extern ALLEGRO_BITMAP *IMAGEM_CARD = NULL;
-extern ALLEGRO_BITMAP *IMAGEM_PERSONAGEM = NULL;
-extern ALLEGRO_EVENT_QUEUE *fila_eventos = NULL;
-extern ALLEGRO_BITMAP *botao_sair = NULL, *area_jogar = 0;
-extern ALLEGRO_EVENT evento;
-extern ALLEGRO_TIMEOUT timeout;
-extern ALLEGRO_AUDIO_STREAM *musica = NULL;
-extern ALLEGRO_SAMPLE *somefeitos = NULL;
-extern ALLEGRO_FONT *fonte = NULL;
-extern ALLEGRO_FONT *fonteSpace = NULL;
-*/
+
 
 ALLEGRO_DISPLAY *janela = NULL;
 ALLEGRO_BITMAP *IMAGEM_SPLASHSCREEN = NULL;
